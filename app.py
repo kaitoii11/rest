82ad086424be9d996c7289ecc432a0f1df13add4
kaitoii11 <kaitoii1111@gmail.com>
2017-11-26 00:53:05 +0900

    added simple api to list files in dir

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
+
+import os
 
 @app.route('/')
 def hello_world():
     return 'Hello'
+
+
+@app.route('/api/')
+@app.route('/api/<dir>')
+def ls(dir='.'):
+    files = os.listdir(dir)
+    return render_template('layout.html',files=files)
 
 
 if __name__ == '__main__':
